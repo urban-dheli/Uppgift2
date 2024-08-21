611636d6f4e2b3e52264e8b94f27369f4db41f4d
Alfred Lundström <alfred.lundstrom@elev.ga.ntig.se>
2024-08-21 09:44:08 +0200

    klart :)

--- analyze_sales.py
+++ analyze_sales.py
@@ -1,14 +1,18 @@
 import csv
 import os
 import locale
+from collections import Counter
+
 
 def analyze_sales_data(filename):
-    products = {}
+    products = {} # bibliotek
+    all_products = [] # lista
 
     with open(filename, 'r') as file:
         reader = csv.DictReader(file)
         for row in reader:
             product = row['Product']
+            all_products.append(product)
             sales = float(row['Sales'])
             
             if product in products:
@@ -16,15 +20,17 @@ def analyze_sales_data(filename):
             else:
                 products[product] = sales
     
-    #TODO: Hitta den mest sålda produkten (TIPS! Använd Counter från collections)
-    
+    # Hitta den mest sålda produkten
+    most_sold_products = Counter(all_products).most_common(1)[0]
+    product_count = most_sold_products[1]
+        
     # Hitta den mest lukrativa produkten
     most_lucrative_product = max(products, key=products.get)
     
     # Genomsnittlig försäljning per produkt
     average_sales = sum(products.values()) / len(products)
 
-    print(f"Mest sålda produkt: ??, Antal: ??")  #FIXME: Redovisa mest sålda produkt här
+    print(f"Mest sålda produkt: {most_sold_products[0]}, Antal: {product_count}")
     print(f"Mest lukrativa produkt: \"{most_lucrative_product}\" med försäljning på {locale.currency(products[most_lucrative_product],grouping=True)}")
     print(f"Genomsnittlig försäljning per produkt: {locale.currency(average_sales, grouping=True)}")
 
